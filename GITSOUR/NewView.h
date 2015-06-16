90ed1c0388d87b1ed30d2a71228ca986324078cf
SudhaPatilI <spiosdeveloper@gmail.com>
2015-06-16 15:59:54 +0530

    New Class Added

---
+++ GITSOUR/NewView.h
@@ -0,0 +1,13 @@
+//
+//  NewView.h
+//  GITSOUR
+//
+//  Created by Manjit on 16/06/15.
+//  Copyright (c) 2015 Manjit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewView : UIViewController
+
+@end
