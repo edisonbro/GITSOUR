ee1d140db5e6713eba3b83c94bba743a27e33b82
SudhaPatilI <spiosdeveloper@gmail.com>
2015-06-16 15:26:46 +0530

    Initial Commit

---
+++ GITSOUR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GITSOUR
+//
+//  Created by Manjit on 16/06/15.
+//  Copyright (c) 2015 Manjit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
